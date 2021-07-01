b10e0d1d2951a66d19c5d4694c5a565710cbb8ea
iflamei <iflamei@mail.ru>
2021-07-01 13:13:25 +0300

    Heroku deploy 5.0

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, '../chat-front/build')));
   // Handle React routing, return all requests to React app
-  app.get('*', function (req, res) {
+  app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../chat-front/build', 'index.html'));
   });
 }
