64dc8af0ea1f3a4f9d3d63944a79c47aaae61a92
iflamei <iflamei@mail.ru>
2021-07-01 13:58:35 +0300

    Heroku deploy 5.0

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import express from 'express';
 const __dirname = path.resolve();
 dotenv.config();
 
-import './core/db.js';
-import createRoutes from './core/routes.js';
-import createSocket from './core/socket.js';
+import './src/core/db.js';
+import createRoutes from './src/core/routes.js';
+import createSocket from './src/core/socket.js';
 
 const app = express();
 const http = createServer(app);
@@ -19,12 +19,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'chat-front/build')));
   // Handle React routing, return all requests to React app
   app.get('/', function (req, res) {
-    res.sendFile(path.join(appRoot, 'chat-front/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'chat-front/build', 'index.html'));
   });
 }
 
 const PORT = process.env.PORT || 3003;
 
-http.listen(PORT, function () {
+http.listen(PORT, '0.0.0.0', function () {
   console.log(`Server: http://localhost:${PORT}`);
 });
